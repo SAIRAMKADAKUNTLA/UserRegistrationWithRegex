9e77d787af3384c38a026213ac18d1ca4b2ad4d0
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-16 22:22:23 +0530

    [Sairam]Added:adding the first name with capital letter using regex

---
+++ UserRegistration/UserRegistration/CkeckValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    class CkeckValidation
+    {
+        public void CheckName(string data)
+        {
+            string validateFirstName = "^[A-Z][a-z]{3,20}";
+            {
+                if (Regex.IsMatch(data,validateFirstName))
+                Console.WriteLine("Name valide succesfully");
+                else
+                    Console.WriteLine("Invalid name");
+            }
+        }
+    }
+}
---
+++ UserRegistration/UserRegistration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User registration with the regex");
+            CkeckValidation check = new CkeckValidation();
+            check.CheckName("Sairam");
+
+        }
+    }
+}
